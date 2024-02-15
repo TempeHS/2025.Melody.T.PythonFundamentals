f3583fe474742c3fe05687925ae88145c6fa654b
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-02-15 22:34:44 +0000

    fin enstein

--- 0-FunctionsVariables/einstein/einstein.py
+++ 0-FunctionsVariables/einstein/einstein.py
@@ -1,10 +1,3 @@
-def main():
-    mass = int(input("Enter mass of object in kg: "))
-    print("Energy of this object is ", energy(mass))
-
-
-def energy(n):
-    x = mass * 
-
-
-main()
+mass = int(input("Enter mass of object in kg: "))
+si = mass * 300000000**2
+print("Energy of this object is", si)
