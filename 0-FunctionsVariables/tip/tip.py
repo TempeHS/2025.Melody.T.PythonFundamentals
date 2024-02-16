54300715a465fa83dc4f2840c59a6a465bb1eca0
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-02-16 10:37:07 +0000

    commi

--- 0-FunctionsVariables/tip/tip.py
+++ 0-FunctionsVariables/tip/tip.py
@@ -1,16 +1,16 @@
 def main():
-		dollars = dollars_to_float(input("How much was the meal? "))
-		percent = percent_to_float(input("What percentage would you like to tip? "))
-		tip = dollars * percent
-		print(f"Leave ${tip:.2f}")
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
 
 
-	def dollars_to_float(d):
-		# TODO
+def dollars_to_float(d):
+    d = d.replace("$", "")
 
 
-	def percent_to_float(p):
-		# TODO
+def percent_to_float(p):
+    p = p.replace("%", "")
 
 
-	main()
+main()
