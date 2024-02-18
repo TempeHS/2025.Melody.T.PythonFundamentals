4cef10aeba136e32a44e564fe999c72269f221f6
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-02-18 10:10:06 +0000

    commit

---
+++ 1-Conditionals/tutorials/house.py
@@ -0,0 +1,14 @@
+# harry potter house sorter based on name
+
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+elif name == "Ron":
+    print("Gryffindor"
+else:
+    print("Who? ")
