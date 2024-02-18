49ea17fe41a878673c0eaaef04ed2e81349efe98
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-02-18 09:00:33 +0000

    commit

--- 1-Conditionals/tutorials/parity.py
+++ 1-Conditionals/tutorials/parity.py
@@ -1,8 +1,17 @@
 # parity: is this number even or odd
 
-x = int(input("Input number: "))
 
-if x % 2 == 0:
-    print("This number is even. ")
-else:ti
-    print("This number is odd. ")
+def main():
+    x = int(input("Input number: "))
+
+    if is_even(x):
+        print("This number is even. ")
+    else:
+        print("This number is odd. ")
+
+
+def is_even():
+    if x % 2 == 0:
+        is_even == True
+    else:
+        is_even == False
