e8f542ea1f8827566f1f2ef048dd7106f5ee5fce
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-02-18 10:22:04 +0000

    commi

--- 1-Conditionals/tutorials/house.py
+++ 1-Conditionals/tutorials/house.py
@@ -2,13 +2,21 @@
 
 name = input("What's your name? ")
 
-if name == "Harry":
-    print("Gryffindor")
-elif name == "Hermione":
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who? ")
+
+
+# alt method
+"""
+if name == "Harry" or "Hermione" or "Ron":
     print("Gryffindor")
 elif name == "Draco":
     print("Slytherin")
-elif name == "Ron":
-    print("Gryffindor"
 else:
     print("Who? ")
+"""
