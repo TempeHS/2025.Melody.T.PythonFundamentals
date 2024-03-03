ff5499658bcc9acab2f9d1b37dc7e96327258936
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-03 01:32:04 +0000

    commi ext.py

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -1,3 +1,21 @@
 #  prompts the user for the name of a file and then outputs that file’s media type
 # .gif .jpg .jpeg .png .pdf .txt .zip
 # no suffix at all: output application/octet-stream
+
+file = input("Input file here: ").lower().strip()
+
+match file:
+    case ".gif":
+        print("PLACEHOLDER")
+    case ".jpg" | "jpeg":
+        print("PLACEHOLDER")
+    case ".png":
+        print("PLACEHOLDER")
+    case ".pdf":
+        print("PLACEHOLDER")
+    case ".txt":
+        print("PLACEHOLDER")
+    case ".zip":
+        print("PLACEHOLDER")
+    case _:
+        print("application/octet-stream")
