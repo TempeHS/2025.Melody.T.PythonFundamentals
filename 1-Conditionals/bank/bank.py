604ec964d89248d063ca8d53f45d54477d91ea3d
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-03 01:21:54 +0000

    fin bank.py

--- 1-Conditionals/bank/bank.py
+++ 1-Conditionals/bank/bank.py
@@ -1,11 +1,11 @@
 #  prompts the user for a greeting. “hello”, output $0. starts with an “h”, output $20. Otherwise, output $100.
 # Ignore whitespace, case-insensitively.
 
-payment = input("Greet me: ").lower
+payment = input("Greet me: ").lower().strip()
 
-if payment == str.startswith("h"):
-    print("$20")
-elif payment == str.startswith("hello"):
+if payment.startswith("hello"):
     print("$0")
+elif payment.startswith("h"):
+    print("$20")
 else:
     print("$200")
