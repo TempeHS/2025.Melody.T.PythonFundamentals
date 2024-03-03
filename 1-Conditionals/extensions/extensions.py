95e16670f321448c3de464fd265f27eac25d0375
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-03 05:04:24 +0000

    fin ext.py

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -4,18 +4,25 @@
 
 file = input("Input file here: ").lower().strip()
 
-match file:
-    case ".gif":
-        print("PLACEHOLDER")
-    case ".jpg" | "jpeg":
-        print("PLACEHOLDER")
-    case ".png":
-        print("PLACEHOLDER")
-    case ".pdf":
-        print("PLACEHOLDER")
-    case ".txt":
-        print("PLACEHOLDER")
-    case ".zip":
-        print("PLACEHOLDER")
-    case _:
-        print("application/octet-stream")
+if file.endswith(".gif"):
+    print("image/gif")
+
+elif file.endswith(".jpg"):
+    print("image/jpeg")
+
+elif file.endswith(".jpeg"):
+    print("image/jpeg")
+
+elif file.endswith(".png"):
+    print("image/png")
+
+elif file.endswith(".pdf"):
+    print("application/pdf")
+
+elif file.endswith(".txt"):
+    print("text/plain")
+
+elif file.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
