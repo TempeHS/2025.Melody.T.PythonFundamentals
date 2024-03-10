1af9b9e5aadccf9917f30b5033143c8e39787927
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-10 01:26:37 +0000

    commi

--- 2-Loops/tutorials/cat.py
+++ 2-Loops/tutorials/cat.py
@@ -1,5 +1,29 @@
-count = 3
-while count != 0:
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number(n):
+    while True:
+        n = input("What's n? ")
+        if n > 0:
+            break
+    return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+"""
+count = 0
+while count < 3:
     print("meow")
-    count = count - 1
-print("end")
+    count = count + 1
+"""
+
+"""
+for i in [0, 1, 2]:
+    print("meow")
+"""
