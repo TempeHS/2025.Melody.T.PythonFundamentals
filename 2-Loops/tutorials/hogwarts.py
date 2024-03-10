33db171b7334b221fd3927f37663e17aa56e753b
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-10 02:24:44 +0000

    commi

---
+++ 2-Loops/tutorials/hogwarts.py
@@ -0,0 +1,8 @@
+data = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+for student in data:
+    print(student, data[student], sep=" - ")
