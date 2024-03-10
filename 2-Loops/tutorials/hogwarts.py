edd4a652d5d74ee40ffc2677c02c94294d90e2ec
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-10 02:42:44 +0000

    com

--- 2-Loops/tutorials/hogwarts.py
+++ 2-Loops/tutorials/hogwarts.py
@@ -1,3 +1,13 @@
+data = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
+for student in data:
+    print(student["name"], student["house"], student["patronus"], sep=": ")
+"""
 data = {
     "Hermione": "Gryffindor",
     "Harry": "Gryffindor",
@@ -6,3 +16,4 @@ data = {
 }
 for student in data:
     print(student, data[student], sep=" - ")
+"""
