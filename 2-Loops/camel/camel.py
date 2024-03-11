d69abc51fb40171a190ba39e95362768076421b2
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-11 00:19:25 +0000

    camel.py commi

---
+++ 2-Loops/camel/camel.py
@@ -0,0 +1,14 @@
+# prompts the user for the name of a variable in camel case and outputs the corresponding name in snake case.
+
+
+def main():
+    input = input("Input camel case here: ")
+    list[input]
+
+
+def convert(i):
+    for i in list:
+        print(i, end="")
+
+
+main()
