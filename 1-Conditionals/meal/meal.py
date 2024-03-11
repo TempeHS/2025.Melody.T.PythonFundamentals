9c59398bdc4641828b9a754f3fc26f2e97ff03ea
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-11 01:21:59 +0000

    fixed meal.py

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,31 +1,19 @@
 # prompts the user for a time and outputs whether it’s breakfast time, lunch time, or dinner time
 # 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00
 
-def main():
-	time = input("What time is it? ")
-	hours, minutes = time.split(":")
-	
-    if 7.00 <= beep <= 8.00:
-        print("brekkie")
-	elif 12.00 <= beep <= 13.00:
-        print("lunch")
-	elif 18.00 <= beep <= 19.00:
-        print("dinner")
-	else:
-		print("nothi")
-	
-def convert(time):
-	beep = float(hours)
-	boop = float(minutes / 60)
-	
-    
 
+def main():
+    time = input("What time is it? ")
+    hours, minutes = time.split(":")
+    hours = float(hours)
+    if 7.00 <= hours <= 8.00:
+        print("time for brekkie")
+    elif 12.00 <= hours <= 13.00:
+        print("time for lunch")
+    elif 18.00 <= hours <= 19.00:
+        print("time for dinner")
+    else:
+        print("nothing")
 
 
 main()
-
-
-
-
-
-
