3be766eeab595bc50568a9032bec3604723103e9
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-11 01:24:00 +0000

    commi

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,5 +1,7 @@
-# prompts the user for a time and outputs whether it’s breakfast time, lunch time, or dinner time
-# 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00
+"""
+prompts the user for a time and outputs whether it’s breakfast time, lunch time, or dinner time
+7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00
+"""
 
 
 def main():
