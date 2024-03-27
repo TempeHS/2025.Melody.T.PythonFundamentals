3b1d04dfe67c65f874dc4f1e2f71550c4c3f81ba
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-03-27 02:48:50 +0000

    com

--- textmenu.py
+++ textmenu.py
@@ -1,1 +1,25 @@
 # textmenu
+
+x = True
+while x == True:
+    print("1. First Item", end="\n")
+    print("2. Second Item", end="\n")
+    print("3. Third item", end="\n")
+    print("4. Exit", end="\n")
+    w = input("What do you want to do? ")
+    x = False
+
+    if w == "1":
+        print("You picked: the first item!")
+        x = True
+
+    if w == "2":
+        print("You picked: the second item!")
+        x = True
+
+    if w == "3":
+        print("You picked: the third item!")
+        x = True
+
+    if w == "4":
+        print("You picked: Exit! Thank you for playing.")
