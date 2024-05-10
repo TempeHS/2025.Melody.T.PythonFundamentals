4f11e57c40bdf31778dc630af66dcb867b5f9e0d
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-05-10 03:54:07 +0000

    cc

--- 8-OOP/tutorials/student.py
+++ 8-OOP/tutorials/student.py
@@ -1,3 +1,12 @@
-name = input("Name: ")
-house = input("Name: ")
-print(f"{name} from {house}")
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+
+def get_name():
+    return input("Name: ")
+
+
+def get_house():
+    return input("House: ")
