f0debc0833ca8cc66d5054c83de013c4765e36e7
Melody Trojanowski <124547562+staletoast@users.noreply.github.com>
2024-05-13 02:14:10 +0000

    cc

--- 8-OOP/tutorials/student.py
+++ 8-OOP/tutorials/student.py
@@ -7,10 +7,9 @@ def main():
 
 # dict
 def get_student():
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
 
 
 # if __name__ == "__main__":
